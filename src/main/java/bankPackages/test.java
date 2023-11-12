8418471266b5e499effece0f29983310ee206f60
rocketpowers <rocketpowerswillian@gmail.com>
2023-11-12 09:21:07 -0300

    first refactor

--- src/main/java/bankPackages/test.java
+++ src/main/java/bankPackages/test.java
@@ -13,7 +13,7 @@ public class test {
 		simpleAccount.setNumberAccount("01");
 		
 		
-		//usando metodo do deposito mais metodo concicional de saque
+		//usando metodo do deposito mais metodo condicional de saque
 		simpleAccount.deposit(100);
 		makeWithdraw(simpleAccount, 40);
 		makeWithdraw(simpleAccount, 70);
@@ -21,11 +21,11 @@ public class test {
 		 System.out.println(simpleAccount);
 
 
-		if (simpleAccount.withdraw(40)) {
-			System.out.println("withdraw sucessfull, new balance = " + simpleAccount.getBalance());
-		} else {
-			System.out.println("insufficient balance to withdraw ");
-		}
+	//	if (simpleAccount.withdraw(40)) {
+		//	System.out.println("withdraw sucessfull, new balance = " + simpleAccount.getBalance());
+	//	} else {
+		//	System.out.println("insufficient balance to withdraw ");
+		//}
 
 	
 	}
